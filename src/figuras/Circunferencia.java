3041e8852d5e7c468a60d09a8919e123c45ef54c
Eli <Eli@DESKTOP-GJVO52H>
2024-03-31 17:46:32 +0200

    0.9.12 Cambiar PI

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -20,12 +20,11 @@ public class Circunferencia {
         color = "rojo";
         System.out.println("Di�metro: " + 2 * radio);
         System.out.println("Color: " + color);
-        double area1 = 2 * PI * radio * radio;
+        double area1 = 2 * 3.1416 * radio * radio;
         double area = area1;
         System.out.println(area);
     }
 
-    private static final double PI = 3.1416;
     private String color;
 
     public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
