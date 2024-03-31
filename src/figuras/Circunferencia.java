4b3f2cd5a439a294ed2d0105b4eef5ab52e5ee5a
Eli <Eli@DESKTOP-GJVO52H>
2024-03-31 16:17:32 +0200

    0.9.3. Renombrar Circulo por Circunferencia

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -1,10 +1,10 @@
 package figuras;
 
-public class Circulo {
+public class Circunferencia {
 
     private double rad;
 
-    public Circulo(double radio) {
+    public Circunferencia(double radio) {
         this.rad = radio;
     }
 
@@ -24,7 +24,7 @@ public class Circulo {
         System.out.println(area);
     }
 
-    public boolean esIgual(Circulo otro, boolean conDecimales) {
+    public boolean esIgual(Circunferencia otro, boolean conDecimales) {
         double radio1 = this.rad;
         double radio2 = otro.getRad();
         if (conDecimales) {
